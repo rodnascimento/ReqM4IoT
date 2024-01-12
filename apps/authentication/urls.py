e3db6786ab91786af939d3360d76b808b7d93916
rodnascimento <rodignascimento@gmail.com>
2024-01-12 11:33:44 -0300

    Objetos de Modelagem nece

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,11 +4,17 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
+from . import views
 
 urlpatterns = [
-    path('login/', login_view, name="login"),
-    path('register/', register_user, name="register"),
+    path('save-graph-data/', views.save_graph_data, name='save_graph_data'),
+    path('load-graph-data/', views.load_graph_data, name='load_graph_data'),
+    # outras URLs da sua aplicação
+]
+
+urlpatterns = [
+    path('login/', views.login_view, name="login"),
+    path('register/', views.register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout")
 ]
